aeb66f7da38a1508f304dfc7b1a51a7e51ecadc0
maciejgaleja <maciej.galeja@outlook.com>
2018-07-21 18:49:44 +0200

    Added test-app body

--- cuavcan_config.h
+++ cuavcan_config.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#define CUAVCAN_MAX_SUBSCRIBED_MESSAGES		3
+#define CUAVCAN_MAX_SUBSCRIBED_MESSAGES				3
 
 #include <stdio.h>
-#define CUAVCAN_DEBUG(fmt, ...) printf(fmt, ##__VA_ARGS__);printf("\n");
+#define CUAVCAN_DEBUG(fmt, ...)						printf(fmt, ##__VA_ARGS__);printf("\n");
+#define CUAVCAN_DEBUG_NO_NEWLINE(fmt, ...)			printf(fmt, ##__VA_ARGS__);
